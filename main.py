68c04a5d734ad140dd2dddc7a97d398e7a636c72
taka_rl <takaya.murakami.uni@gmail.com>
2025-05-20 15:13:49 +0200

    Draw Mario and display it in the window

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ TILE_X = 16
 TILE_Y = 14
 
 
+class Mario(pygame.sprite.Sprite):
+    '''Mario class'''
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Load mario images
+        self.__imgs = [
+            pygame.image.load('./img/mario_1.jpg'),
+        ]
+        
+        self.image = self.__imgs[0]
+        self.rect = pygame.Rect(150, 180, 20, 20)
+        
+
+
 def main():
     '''main function'''
     
@@ -21,7 +37,16 @@ def main():
     # Create clock rate
     clock = pygame.time.Clock()
     
-        
+    # Define Sprite group
+    group = pygame.sprite.RenderUpdates()
+    
+    # Mario class
+    mario = Mario()
+    
+    # Add mario into the group
+    group.add(mario)
+    
+
     # Event loop
     running = True
     while running:
@@ -31,6 +56,12 @@ def main():
 
         # Fill in the background     
         win.fill((135, 206, 235))
+        
+        # Update the group
+        group.update()
+        
+        # Draw the group
+        group.draw(win)
                 
         # Update the display
         pygame.display.flip()
